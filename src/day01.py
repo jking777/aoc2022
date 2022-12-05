6e0537783aa925a8bcf189b7c1f06e8fe62f40ec
Jim King <jim.king@bluware.com>
2022-12-05 10:39:08 -0600

    day 1 final, plus Pycharm files

--- src/day01.py
+++ src/day01.py
@@ -1,6 +1,6 @@
 from data.day01_data import sample_lines, input_lines
 
-sample = True
+sample = False
 if sample:
     data = sample_lines
 else:
@@ -8,7 +8,35 @@ else:
 
 
 def main():
-    pass
+    elf_totals = {}
+    elf_num = 1
+    elf_max_num = 0
+    elf_max = -1
+    for line in data:
+        if len(line) == 0:
+            elf_num += 1
+        else:
+            elf_total = elf_totals.setdefault(elf_num, 0)
+            elf_total += int(line)
+            elf_totals[elf_num] = elf_total
+
+            if elf_total > elf_max:
+                elf_max = elf_total
+                elf_max_num = elf_num
+
+    print(f"Elf # {elf_max_num} has {elf_max} calories")
+
+    # part 2
+
+    sorted_elves_by_total = sorted(elf_totals.items(), key=lambda x: x[1])
+    print()
+    print(f"Top 3 elves:")
+    top3_total = 0
+    for i in [-1, -2, -3]:
+        elf_info = sorted_elves_by_total[i]
+        print(f"Elf {elf_info[0]} has {elf_info[1]} calories")
+        top3_total += elf_info[1]
+    print(f"Top 3 total calories:  {top3_total}")
 
 
 if __name__ == "__main__":
